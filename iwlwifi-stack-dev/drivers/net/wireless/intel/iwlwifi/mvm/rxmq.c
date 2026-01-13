ef5f3e0cd4446cb8df34a102a409404e6087f801
TOBETA <35523848+MasakazuTobeta@users.noreply.github.com>
2026-01-13 12:07:21 +0900

    iwlwifi: drop unknown-encryption RX frames

--- iwlwifi-stack-dev/drivers/net/wireless/intel/iwlwifi/mvm/rxmq.c
+++ iwlwifi-stack-dev/drivers/net/wireless/intel/iwlwifi/mvm/rxmq.c
@@ -400,7 +400,12 @@ static int iwl_mvm_rx_crypto(struct iwl_mvm *mvm, struct ieee80211_sta *sta,
 	    IWL_RX_MPDU_STATUS_SEC_NONE)
 		return 0;
 
-	/* TODO: handle packets encrypted with unknown alg */
+	if ((status & IWL_RX_MPDU_STATUS_SEC_MASK) ==
+	    IWL_RX_MPDU_STATUS_SEC_UNKNOWN) {
+		if (!mvm->monitor_on && net_ratelimit())
+			IWL_WARN(mvm, "Unhandled alg: 0x%x\n", status);
+		return -1;
+	}
 
 	switch (status & IWL_RX_MPDU_STATUS_SEC_MASK) {
 	case IWL_RX_MPDU_STATUS_SEC_CCM:
